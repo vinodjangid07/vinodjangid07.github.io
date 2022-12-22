d782cdd9db85b154a20264a7b9c972587cd5ee84
vinod <infovinodjangid@gmail.com>
2022-12-22 09:08:45 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,82 @@
+// preloader script............
+var audio = document.getElementById("audioPlayer");
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+})
+
+// preloader script ends here.........
+
+
+function playpause() {
+  if (document.getElementById('switch').checked == false) {
+
+    audio.pause();
+      
+   }
+
+ else{
+     
+    audio.play();
+     
+    }
+    
+  }
+
+
+
+let emptyArea = document.getElementById("emptyarea");
+// toggle menu by clicking on hamburger
+function hamburgerMenu() {
+    document.body.classList.toggle("stopscrolling");
+document.getElementById("mobiletogglemenu").classList.toggle("show-toggle-menu");
+document.getElementById("burger-bar1").classList.toggle("hamburger-animation1");
+document.getElementById("burger-bar2").classList.toggle("hamburger-animation2");
+document.getElementById("burger-bar3").classList.toggle("hamburger-animation3");
+}
+// close mobile toggle menu by clicking on LI
+function hidemenubyli(){
+document.body.classList.toggle("stopscrolling");
+document.getElementById("mobiletogglemenu").classList.toggle("show-toggle-menu");
+document.getElementById("burger-bar1").classList.toggle("hamburger-animation1");
+document.getElementById("burger-bar2").classList.toggle("hamburger-animation2");
+document.getElementById("burger-bar3").classList.toggle("hamburger-animation3");
+}
+
+// close Mobile hamburger toggle menu while clicking in empty area
+emptyArea.addEventListener('click',hidetogglemenu);
+function hidetogglemenu(){
+document.body.classList.remove("stopscrolling");
+document.getElementById("mobiletogglemenu").classList.remove("show-toggle-menu");
+document.getElementById("burger-bar1").classList.remove("hamburger-animation1");
+document.getElementById("burger-bar2").classList.remove("hamburger-animation2");
+document.getElementById("burger-bar3").classList.remove("hamburger-animation3");
+}
+
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('.navbar .navbar-tabs .navbar-tabs-ul a li');
+const mobilenavLi = document.querySelectorAll('.mobiletogglemenu .mobile-navbar-tabs-ul a li');
+
+window.addEventListener('scroll', ()=>{
+  let current = "";
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if(pageYOffset >= (sectionTop - 500)){
+      current = section.getAttribute('id');
+    }
+  })
+
+  mobilenavLi.forEach( li => {
+    li.classList.remove('activeThismobiletab');
+    if(li.classList.contains(current)){
+      li.classList.add('activeThismobiletab')
+    }
+  })
+navLi.forEach( li => {
+  li.classList.remove('activeThistab');
+  if(li.classList.contains(current)){
+    li.classList.add('activeThistab')
+  }
+})
+})
