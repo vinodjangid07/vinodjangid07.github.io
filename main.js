4c80ee96083a20aaa0c4d453bd9c749a4e53d13c
vinod <infovinodjangid@gmail.com>
2023-01-01 12:26:44 +0530

    countless commit

--- main.js
+++ main.js
@@ -125,7 +125,7 @@ navLi.forEach( li => {
   }
 })
 })
-console.log('%c Designed and Developed by Vinod Jangid ', 'background: #8000ff; color: white; padding:20px;');
+console.log('%c Designed and Developed by Vinod Jangid ', 'background-image: linear-gradient(90deg,#8000ff,#6bc5f8); color: white;font-weight:900;font-size:1rem; padding:20px;');
 
 
 
